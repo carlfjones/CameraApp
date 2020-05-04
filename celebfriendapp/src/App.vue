8e9cff6da648683b15606c6d204a51a928769c19
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-04 12:01:19 +0100

    Add Camera Vue

---
+++ celebfriendapp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Camera />
+  </div>
+</template>
+
+<script>
+import Camera from './components/Camera';
+
+export default {
+  name: 'App',
+  components: {
+    Camera
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #F3F3F3;
+}
+</style>
---
+++ celebfriendapp/src/components/Camera.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="camera">
+        <video autoplay class="feed"></video>
+        <button class="snap">Snap</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "camera"
+}
+</script>
+
+<style lang="scss" scoped>
+.camera {
+    width: 100vw;
+    height: 100vh;
+
+    .feed {
+        width:100%;
+        max-width: 1280px;
+        box-shadow: 4px 4px 12px 0px rgba(0, 0, 0, 0.25);
+    }
+}
+</style>
