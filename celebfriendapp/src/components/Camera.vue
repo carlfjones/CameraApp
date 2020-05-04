f7b3bac98808562627066d4b4c192c9a1227a92c
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-04 12:36:23 +0100

    Add Media Device camera

--- celebfriendapp/src/components/Camera.vue
+++ celebfriendapp/src/components/Camera.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="camera">
         <video autoplay class="feed"></video>
-        <button class="snap">Snap</button>
+        <button class="snap">SNAP</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: "camera"
+    name: "camera",
+    methods: {
+        init() {
+            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+                navigator.mediaDevices.getUserMedia({video:true}).then(stream => {
+                   const videoPlayer = document.querySelector("video");
+                   videoPlayer.srcObject = stream;
+                   videoPlayer.play();
+                });
+            } else {
+                alert("Cannot get Media Devices");
+            }
+        }
+    },
+    beforeMount() {
+        this.init();
+    }
 }
 </script>
 
@@ -15,11 +31,36 @@ export default {
 .camera {
     width: 100vw;
     height: 100vh;
+    padding: 25px;
 
     .feed {
+        box-sizing: border-box;
+        background-color: #161616;
+        display: block;
+        margin: 0 auto;
         width:100%;
         max-width: 1280px;
-        box-shadow: 4px 4px 12px 0px rgba(0, 0, 0, 0.25);
+        box-shadow: 6px 6px 12px 0px rgba(0, 0, 0, 0.35);
+    }
+
+    .snap {
+        margin: 25px auto;
+        display: block;
+        color: #08718b;
+        width: 75px;
+        height: 75px;
+        border-radius: 50%;
+        background-color: transparentize($color: #7a7a7a, $amount: 0.75);
+        border: 1px solid #171717;
+        outline: none;
+        cursor: pointer;
+
+        &:hover {
+            background-color: transparentize($color: #177f86, $amount: 0.2);
+        }
+        &:active {
+            background-color: darken($color: #177f86, $amount: 0.5);
+        }
     }
 }
 </style>
